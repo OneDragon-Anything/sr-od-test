7a0587a52303b20a83576d60f0ea84f8e5195aa3
DoctorReid <doctorreid2024@outlook.com>
2024-11-30 15:30:13 +0800

    #392 完善到背包遗器画面的跳转

---
+++ test/sr_od/screen_state/test_get_match_screen_name/test_get_match_screen_name.py
@@ -0,0 +1,25 @@
+from one_dragon.base.screen import screen_utils
+from test import SrTestBase
+
+
+class TestGetMatchScreenName(SrTestBase):
+
+    def __init__(self, *args, **kwargs):
+        SrTestBase.__init__(self, *args, **kwargs)
+
+    def test_get_match_screen_name(self):
+        screen_map = {
+            'normal_world_basic': '大世界-普通',
+            'bag_upgrade_material': '背包-养成材料',
+            'bag_light_cone': '背包-光锥',
+            'bag_relic': '背包-遗器',
+            'bag_other_material': '背包-其他材料',
+            'bag_consumable': '背包-消耗品',
+            'bag_mission': '背包-任务',
+            'bag_valuable': '背包-贵重物',
+            'bag_pet': '背包-随宠'
+        }
+        for image_name, screen_name in screen_map.items():
+            screen = self.get_test_image(image_name)
+            result = screen_utils.get_match_screen_name(self.ctx, screen)
+            self.assertEqual(result, screen_name)
